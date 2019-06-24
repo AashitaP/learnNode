ee31bfe2480f48d77bbc2a504de39a0b91d3672d
Aashita <harshupatwari@gmail.com>
2019-06-24 14:54:24 -0700

    rest api

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,27 +5,29 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/'; //where mongodb server can be accessed
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => { //callback function, fills parameters
-    assert.equal(err, null);
+MongoClient.connect(url).then((client) => { //callback function, fills parameters 
+    //with promises, all the callback functions are put into .then functions, returns the promise within the function, & chains & error is .catch
     console.log('Connected correctly');
 
     const db = client.db(dbname);
-    const collection = db.collection('dishes'); //dishes collection of documents
-    dboper.insertDoc(db, {"name": "Pizza 1", "description": "test 2"}, 'dishes', (result) => {
+    dboper.insertDoc(db, {"name": "Pizza 1", "description": "test 2"}, 'dishes')
+    .then((result) => {
         console.log('After Insert: ', result.ops); //how many ops have been carried out successfully
-        dboper.insertDoc(db, {"name": "Pizza 2", "description": "test 2"}, 'dishes', (result) => {
-            dboper.findDoc(db, 'dishes', (docs) => { //ensures operation is completed, by putting it inside callback function
+        return dboper.findDoc(db, 'dishes');
+        })
+        .then((docs) => { //ensures operation is completed, by putting it inside callback function
             console.log('Found: ');
             console.log(docs);
-            dboper.updateDoc(db, {name: "Pizza 1"}, {description: 'updated test'}, 'dishes', (result)=> {
+            return dboper.updateDoc(db, {name: "Pizza 1"}, {description: 'updated test'}, 'dishes');
+            })
+            .then((result)=> {
                 console.log('Updated document: ', result.result);
-                db.dropCollection('dishes', (err, result) => {
+                return db.dropCollection('dishes');
+                })
+                .then((result) => {
                     console.log("Dropped.", result);
-                    assert.equal(err, null);
-                    client.close();
-                });
-            });
-        });
-    });
-});
-});
+                    return client.close();
+                })
+                .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,38 +1,46 @@
 const assert = require('assert');
 
-exports.insertDoc = (db, doc, collection, callback) => {
+exports.insertDoc = (db, doc, collection, callback) => { //callback function is essentially a function that is called at the end of everything with the result
     const coll = db.collection(collection);
-    coll.insert(doc, (err, result) => {
+    return coll.insert(doc);
+        
+    /*(err, result) => {
         assert.equal(err, null);
         console.log("Inserted " + result.result.n + "into" + collection);
         callback(result);
-    });
+    });*/
 
 };
 
 exports.findDoc = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find().toArray((err, docs) => {
+    return coll.find().toArray();
+        
+    /*(err, docs) => {
         assert.equal(err, null);
         callback(docs);
-    });
+    });*/
 };
 
 exports.removeDoc = (db, doc, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(doc, (err, result) => {
+    return coll.deleteOne(doc);
+        
+    /*(err, result) => {
         assert.equal(err, null);
         console.log("Removed document ", document); //comma if its a javascript object
         callback(result);
-    });
+    });*/
 
 };
 
 exports.updateDoc = (db, doc, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(doc, {$set: update}, null, (err, result) => {
+    return coll.updateOne(doc, {$set: update}, null);
+        
+    /*(err, result) => {
         assert.equal(err, null);
         console.log("updated doc with ", update);
         callback(result);
-    });
+    });*/
 };
